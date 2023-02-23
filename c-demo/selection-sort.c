80d39912f99a878b3929044ac43f1647b653e0b2
Dharun Anandayuvaraj <dharun99@gmail.com>
2023-02-23 13:16:05 -0500

    Update selection-sort.c

--- c-demo/selection-sort.c
+++ c-demo/selection-sort.c
@@ -1,44 +1,25 @@
 #include <stdio.h>
 
 void selection_sort(int arr[], int n) {
-    int i, j, min_idx;
-    for (i = 0; i < n-1; i++) {
-        min_idx = i;
-        for (j = i+1; j <= n; j++) {
-            if (arr[j] < arr[min_idx]) {
+    for (int i = 0; i < n-1; i++) {
+        int min_idx = i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] > arr[min_idx])
                 min_idx = j;
-            }
         }
-        // swap
-        int temp = arr[min_idx];
-        arr[min_idx] = arr[i];
-        arr[i] = temp;
-
-        printf("Current sorted array: ");
-        for (int k = 0; k <= n; k++) {
-            printf("%d ", arr[k]);
-        }
-        printf("\n");
+        swap(arr[i], arr[min_idx]);
     }
 }
 
+void swap(int *xp, int *yp) {
+    *xp = *yp;
+    *yp = temp;
+}
+
 int main() {
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Original array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    selection_sort(arr, n);
-
-    printf("Sorted array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
+    int arr[5] = {4, 2, 9, 7, 1};
+    selection_sort(arr, 5);
+    for (int i = 0; i < 5; i++)
+        printf("%d , arr[i]);
     return 0;
 }
